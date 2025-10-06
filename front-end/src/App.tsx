3bc5e0a2bd2870d837699339db3287d075d512a1
Kayky Ribeiro <kaykykaro04@gmail.com>
2025-10-06 16:25:07 -0300

    criação do app

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -17,7 +17,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>front-end</h1>
+      <h1 className='text-blue-500'>front-end</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}

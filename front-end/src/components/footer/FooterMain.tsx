424e6094d86c54d22b4dd8e710068b40db88ec1f
Kayky Ribeiro <kaykykaro04@gmail.com>
2025-12-03 14:33:19 -0300

    V3.9.3 - correção de documento

--- front-end/src/components/footer/FooterMain.tsx
+++ front-end/src/components/footer/FooterMain.tsx
@@ -29,7 +29,7 @@ export default function footerMain() {
                     onClick={() => navigate("/termos-de-uso")}>Termos de uso</p>
                 <p>Protótipo do PI grupo Drive Parts</p>
                 <p>© 2025 DriveParts. Todos os direitos reservados.</p>
-                <p>Versão: 3.2.0</p> 
+                <p>Versão: 3.9.3</p> 
             </div>
             
         </div>

6eacfc84d1795afd2945acc7501149041f9b44cf
João Pedro <joaoprl055@gmail.com>
2026-06-01 11:07:42 -0300

    Correção dos logs

--- front-end/src/pages/Login.tsx
+++ front-end/src/pages/Login.tsx
@@ -69,10 +69,8 @@ export default function Login() {
       localStorage.setItem("token", "")
       const response = await VerifyLogin(form.telefone, form.senha);
       localStorage.setItem("token", response.data.access_token);
-      console.log("TOKEN: ", localStorage.getItem("token"));
       localStorage.setItem("usu_tipo", response.data.user.usu_tipo);
       localStorage.setItem("user", JSON.stringify(response.data.user));
-      console.log(localStorage.getItem("user"));
 
       if (redirect) {
         navigate(redirect);
